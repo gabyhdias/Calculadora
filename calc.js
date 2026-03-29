c6646fd18b0e1639d082dea46a9226707b4312d8
GabrielaHdias <gabriela_h_dias@estudante.sesisenai.org.br>
2026-03-29 14:58:33 -0300

    add

--- calc.js
+++ calc.js
@@ -1,3 +1,25 @@
-function calculadora {
-    
-} 
+function calculadora(){
+    let texto = document.getElementById("valor1").value;
+    let tudo = texto.split(/([+\-*/])/).map(item => item.trim());
+    let resultado;
+
+    const n1 = Number(tudo[0]);
+    const operador = tudo[1];
+    const n2 = Number(tudo[2]);
+
+    if (operador === "+") {
+      resultado = n1 + n2;
+      document.getElementById("resultado").innerText = "Resultado: " + resultado;
+    } else if (operador === "-"){
+        resultado = n1 - n2;
+        document.getElementById("resultado").innerText = "Resultado: " + resultado;
+    } else if (operador === "*") {
+        resultado = n1 * n2;
+        document.getElementById("resultado").innerText = "Resultado: " + resultado;
+    } else if (operador === "/"){
+        resultado = n1 / n2;
+        document.getElementById("resultado").innerText = "Resultado: " + resultado;
+    } else {
+        document.getElementById("resultado").innerText = "Resultado: Inválido";
+    }
+}
